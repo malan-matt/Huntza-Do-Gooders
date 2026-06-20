5643f194e00b557ea00978c45d8432b03b35f40e
infuser <infuser@INFOE-11.up.ac.za>
2026-06-20 16:13:35 +0200

    loop

--- main.ts
+++ main.ts
@@ -9,14 +9,6 @@ export function isValid(email : string) : boolean {
         if(email[i]==='"') {
         //Bianca loop
         }
-
-
-    
-    
-    
-
-    if (!checkFront(email)) {
-        return false;
     }
     //test for ..
    //only 1 @
@@ -58,33 +50,7 @@ export function isValid(email : string) : boolean {
     return true;
 }
 
-export function validChars(email:string) {
-    var invalidChars = ['(', ')', ',', ':', ';', '<', '>', '[', ']', '\\', '"'];
-    for (var i = 0; i < invalidChars.length; i++) {
-        if (email.includes(invalidChars[i])) {
-            return false;
-        }
-    }
-    for (var i =0; i < email.length;i++) {        
-        if(email[i]==='.' && email[i+1]==='.') {
-            return false;
-        }
-    }
-    return true;
-}
-function checkFront(email:string) : boolean {
-    const atIndex = email.indexOf('@');
-    const frontPart = email.substring(0, atIndex);
 
-    if (frontPart.endsWith('.') || frontPart.startsWith('.')) {
-        return false;
-    }
-    if (frontPart.includes('..')) {
-        return false;
-    }
-
-    return true;
-}
 function checkBack(email:string) : boolean {
     const atIndex = email.indexOf('@');
     const backPart = email.substring(atIndex, email.length -1);
