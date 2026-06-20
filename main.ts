4a715f93efb8632058e48f07e49f02dd21eedd44
malan-matt <malan.matth@gmail.com>
2026-06-20 14:37:56 +0200

    Update

--- main.ts
+++ main.ts
@@ -1,16 +1,26 @@
 export function isValid(email : string) : boolean {
     email = email.toLowerCase();
     email = email.trim();
+    
     if (((email.match(/@/g) || []).length !== 1) || (email.match(/\s/g) || []).length) {
         return false;
     }
     
-    if(email!=='${string}@${string}.{string}') {
+    if((email!=='${string}@${string}.{string}') || (email.startsWith('.')) || (email.endsWith('.')) ){
         return false;
     }
 
+    var invalidChars = ['(', ')', ',', ':', ';', '<', '>', '[', ']', '\\', '"'];
+    for (var i = 0; i < invalidChars.length; i++) {
+        if (email.includes(invalidChars[i])) {
+            return false;
+        }
+    }
     
+    var front = email.split('@')[0];
+    var back = email.split('@')[1];
 
+    
     
     return true;
 }
