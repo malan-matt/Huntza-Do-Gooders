348c12e03d1ee2b914dc6a004da9ef92f67e280c
malan-matt <malan.matth@gmail.com>
2026-06-20 14:13:26 +0200

    email format simple

--- main.ts
+++ main.ts
@@ -1,6 +1,9 @@
 export function isValid(email : string) : boolean {
     email = email.toLowerCase();
     email = email.trim();
+    if (((email.match(/@/g) || []).length !== 1) || ("str1,str2,str3,str4".match(/\s/g) || []).length) {
+        return false;
+    }
 
     return true;
 }
